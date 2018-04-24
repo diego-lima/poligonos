c0a3fc810588b6e4c14b694c25235761d2ee9578
Diego Lima <ddddiegolima@gmail.com>
2018-04-23 23:18:53 -0300

    atualizada documentação

--- html/search/all_a.js
+++ html/search/all_a.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['self_5fintersect',['self_intersect',['../class_poligono.html#a5d5ba5fd150f66f7bda85379f8940960',1,'Poligono']]],
+  ['setar_5flimites',['setar_limites',['../class_mostrador.html#ad648b54d3f842b320318319aae40c051',1,'Mostrador']]],
   ['sub',['sub',['../class_ponto.html#a29db0824c989d122fe33ee23eba176a2',1,'Ponto']]]
 ];
