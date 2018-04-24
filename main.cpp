e8bfdae55040538d6933e0a7cfac1701c4f0c232
Diego Lima <ddddiegolima@gmail.com>
2018-04-23 22:58:55 -0300

    Programa exemplo (etapa 1.4)

--- main.cpp
+++ main.cpp
@@ -10,10 +10,30 @@
 #define NENHUMA -1
 
 int main(int argc, char *argv[]){
-    Poligono poli;
-    poli << Ponto(0,0) << Ponto(1,0) << Ponto(0.5,1);
-    poli.print();
+    //Crie um pequeno exemplo para testar sua implementação da classe Retangulo
+    //No exemplo, defina um novo retângulo na posição (x,y)=(0,0)(x,y)=(0,0)
+    //com altura e largura iguais a 3 e 4, respectivamente.
+    Retangulo retan(0,0,4,3);
 
+    //Imprima a estrutura poligonal gerada para o retângulo.
+    retan.print();
 
+    //Calcule a sua área usando a função já implementada na classe Poligono
+    printf("Area do retangulo: %.1f\n", retan.area());
+
+    //Mude a posição do retângulo usando a função translada(float,float)
+    // para (x,y)=(−3,4) e recalcule a área do retângulo.
+    // Compare-a com a área calculada antes da transformação geométrica.
+    retan.transladar(-3,4);
+    retan.print();
+    printf("Area do retangulo depois do translado: %.1f--->SÃO IGUAIS!\n", retan.area());
+
+    //Rotacione o Retângulo de +30o em relação ao seu centro de massa
+    //e recalcule a sua área. Compare-a com a área calculada antes da
+    //transformação geométrica.
+    retan.rotacionar(30, Ponto(2,-1.5) );
+    printf("Area do retangulo depois da rotação: %.1f--->SÃO IGUAIS!\n", retan.area());
+
+    printf("\n\n\n");
     return 0;
 }
